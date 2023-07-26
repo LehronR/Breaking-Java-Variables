e251a667c78a7327f6231cefe9f0b9132567fc88
Lehron <lehronrobinson@yahoo.com>
2023-07-25 22:26:41 -0400

    Assignment completed.  Tested several scenarios for different error results with variables.

--- src/variableTesting.java
+++ src/variableTesting.java
@@ -1,5 +1,30 @@
 public class variableTesting {
     public static void main(String[] args) {
+//        COMMENTED OUT VARIABLES TO TEST EACH INDIVIDUALLY AND SEE WHAT TYPES OF ERRORS I WOULD RECEIVE.
 
+//        int number;
+//        Still blows my mind that String isn't a reserved keyword and can be used as a variable name lol.
+//        String String = "some text";
+//        byte num = 150;
+//        byte double;
+//        String &word;
+//        String sameName = "First definition of this name.";
+//        String varWithinTwoParentScopes = "First definition of this variable name";
+//        int 2seven = 8;
+
+//        System.out.println(number);
+//        System.out.println(byte);
+//        System.out.println(num);
+//        System.out.println(String);
+//        System.out.println(2seven);
+
+        for(int i = 0; i < 3; i++) {
+            String sameName = "Testing if different scoped variables can have the same name.";
+            System.out.println(sameName);
+            if(true) {
+                String varWithinTwoParentScopes = "Still throws an error, I'm guessing due to variables being able to access variables all the way the the global scope.";
+                System.out.println(varWithinTwoParentScopes);
+            }
+        }
     }
 }
